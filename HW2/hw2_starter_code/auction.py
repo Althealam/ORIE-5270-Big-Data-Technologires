b83950d364f6344f12395a3227150f27bbad67ad
Althea Lam <althealam0531@gmail.com>
2026-03-06 22:02:57 -0500

    hw2 q3 and q4 solution

--- HW2/hw2_starter_code/auction.py
+++ HW2/hw2_starter_code/auction.py
@@ -1,9 +1,16 @@
+# requirement:
+# 1. space: O(k)
+# 2. time: O(logk)
+# 3. not allowed to store the entire bid history
+
+import heapq
 class Auctioneer(object):
     """A class emulating an auctioneer that tracks the highest K bids."""
 
     def __init__(self, K):
         self.K = K
         # TODO: Initialize any data structures or other fields necessary here.
+        self.min_heap = [] # space: O(k)
 
     def process_next_bid(self, bid_value):
         """Process the next bid, updating the highest K bids if necessary.
@@ -12,6 +19,16 @@ class Auctioneer(object):
         of K bids and `False` if not.
         """
         # TODO: Fill in your code here.
+        if len(self.min_heap)<self.K: # time: O(logk)
+            heapq.heappush(self.min_heap, bid_value)
+            return True
+        else:
+            if bid_value>self.min_heap[0]:
+                heapq.heappop(self.min_heap) # time: O(logk)
+                heapq.heappush(self.min_heap, bid_value) # time: O(logk)
+                return True
+        return False
+
 
     def get_bids(self):
         """Return the highest K bids encountered so far in sorted order.
@@ -21,3 +38,4 @@ class Auctioneer(object):
         order.
         """
         # TODO: Fill in your code here.
+        return self.sorted(self.min_heap) # time: O(logk)
--- HW2/hw2_starter_code/mergesort_4.py
+++ HW2/hw2_starter_code/mergesort_4.py
@@ -1,8 +1,49 @@
 def merge_4(A, B, C, D):
     """Merge 4 arrays passed in sorted order."""
     # TODO: Your code here.
+    def merge_2(arr1, arr2):
+        if len(arr1)==0:
+            return arr2
+        if len(arr2)==0:
+            return arr1
+        i, j = 0, 0
+        sorted_arr = []
+        while i<len(arr1) and j<len(arr2):
+            if arr1[i]<arr2[j]:
+                sorted_arr.append(arr1[i])
+                i+=1
+            else:
+                sorted_arr.append(arr2[j])
+                j+=1
+        if i<len(arr1):
+            sorted_arr.extend(arr1[i:])
+        if j<len(arr2):
+            sorted_arr.extend(arr2[j:])
+        return sorted_arr[:]
+    
+    sorted_arr1 = merge_2(A, B)
+    sorted_arr2 = merge_2(C, D)
+    return merge_2(sorted_arr1, sorted_arr2)
+
+
 
 def mergesort_4(arr):
     """Sort `arr` using mergesort with 4 instead of 2 subproblems."""
     # TODO: Your code here. You should use `merge_4` to combine
     # solutions from subproblems.
+    if len(arr)<=1:
+        return arr
+    length = len(arr)
+    q1 = length//4
+    q2 = length//2
+    q3 = 3*length//4
+    A = mergesort_4(arr[:q1])
+    B = mergesort_4(arr[q1:q2])
+    C = mergesort_4(arr[q2:q3])
+    D = mergesort_4(arr[q3:])
+    return merge_4(A, B, C, D)
+
+
+A = [2, 1, 5, 6, 2, 4, 6]
+res = mergesort_4(A)
+print(res)
